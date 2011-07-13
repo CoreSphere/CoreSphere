5f9e2af08519d77220830cba0d5cd18efd1d1481
Laszlo Korte <laszlokorte@gmail.com>
2011-07-13 03:48:09 +0200

    added confirmation dialog to delete link

--- app/Resources/public/js/base.js
+++ app/Resources/public/js/base.js
@@ -1,3 +1,19 @@
-(function() {
+(function(window) {
+    "use strict";
 
-})();
+    var $             = window.jQuery,
+        event_handler = function(event) {
+            var $this = $(this)
+
+            if (!confirm($this.data('confirm'))) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+
+        };
+
+    $(window.document.body)
+
+        .delegate('[data-confirm]', 'click.coresphere', event_handler);
+
+}(window));
